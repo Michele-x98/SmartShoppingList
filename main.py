cccceb21c5eb1dc177961d408dd33936c00cfca1
MicheleBenedetti <45004856+Michele-x98@users.noreply.github.com>
2023-04-06 03:22:27 +0200

    add candidate labels from POST

--- main.py
+++ main.py
@@ -12,20 +12,23 @@ classifier = pipeline("zero-shot-classification",
 
 
 class ClassifyRequest(object):
-    def __init__(self, item, candidate_labels, top_k=3, use_translation=False, t_from='', t_to=''):
+    def __init__(self, item, candidate_labels,  top_k=3, use_translation=False, t_from='', t_to=''):
         self.item = item
-        self.candidate_labels = candidate_labels,
+        self.candidate_labels = candidate_labels
         self.top_k = top_k
         self.use_translation = use_translation
         self.t_from = t_from
         self.t_to = t_to
 
 
-@app.route("/classify")
+@app.route("/classify", methods=['POST'])
 def getClassification():
-    j = json.loads(json.dumps(request.get_json()))
-    c = ClassifyRequest(**j)
-    return _getClassification(c)
+    if request.method == 'POST':
+        j = json.loads(json.dumps(request.get_json()))
+        c = ClassifyRequest(**j)
+        return _getClassification(c)
+    else:
+        return 'Error: only POST requests are supported'
 
 
 def _translate(input, from_, to):
@@ -47,14 +50,14 @@ def _getClassification(c: ClassifyRequest):
     if (c.item == ''):
         return 'Error: missing item to classify'
 
-    labels = c.candidate_labels
-    if (labels.__len__() == 0):
-        with open('candidate_labels.json') as f:
-            # the json file contains an object with a single key "labels" and a list of labels as value
-            labels = json.load(f)['labels']
+    labels = list(c.candidate_labels)
+    print(labels)
+
+    if (labels == ''):
+        return 'Error: missing candidate labels'
 
     print('Item to classify: ', c.item)
-    print('Candidate labels: ', c.candidate_labels)
+    print('Candidate labels: ', labels)
 
     if (c.use_translation == 'false' or c.use_translation == False):
         # classify the input
@@ -71,6 +74,7 @@ def _getClassification(c: ClassifyRequest):
 
         end = time.process_time() - st
         print('Time needed to classify and sort: ', end)
+        print(top3_labels)
 
         return top3_labels
 
